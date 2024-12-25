a08b745fd359a367c9abdb8204cef6d19aff60bf
Thales Bensi <thalesbensi@outlook.com>
2024-12-25 16:31:24 -0300

    Entity Task Created

--- src/main/java/com/thalesbensi/ToDoList/entities/Task.java
+++ src/main/java/com/thalesbensi/ToDoList/entities/Task.java
@@ -1,4 +1,36 @@
 package com.thalesbensi.ToDoList.entities;
 
+import com.thalesbensi.ToDoList.enums.TaskStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Date;
+
+
+@Entity
+@Data
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus Status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateAt;
+
+
 }
---
+++ src/main/java/com/thalesbensi/ToDoList/enums/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.thalesbensi.ToDoList.enums;
+
+public enum TaskStatus {
+    PENDING,
+    COMPLETED
+}
